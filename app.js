8e662478a89a979774ca3fe34d0872b6699d52fe
Boirokk <chadzx10@gmail.com>
2022-04-24 21:08:39 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/register', (req, res) => {
+    res.render('register');
+})
+
+
+
+app.listen(3000);
